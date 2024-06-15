c9636a3ef4a2cb8da4b3d6035d51c43653eccdbe
Amegahy <32388552+Amegahy@users.noreply.github.com>
2024-06-15 17:22:09 +0100

    Update Logged.php

--- app/Tags/Logged.php
+++ app/Tags/Logged.php
@@ -6,12 +6,6 @@ use Statamic\Tags\Tags;
 
 class Logged extends Tags
 {
-    
-    /**
-     * The {{ logged }} tag.
-     *
-     * @return string|array
-     */
     public function index()
     {
         return $_SESSION["logged"];
